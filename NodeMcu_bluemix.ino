51cd39b2aa80aaafe7796e4ee65345cd51827ddf
tarun <taditarun123@gmail.com>
2017-08-12 22:20:50 +0530

    Add files via upload

---
+++ NodeMcu_bluemix.ino
@@ -0,0 +1,68 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h> // https://github.com/knolleary/pubsubclient/releases/tag/v2.3
+
+//-------- Customise these values -----------
+const char* ssid = "connected";
+const char* password = "anvesh1998";
+
+#define ORG "shgg2s"
+#define DEVICE_TYPE "ESP8266"
+#define DEVICE_ID "5CCF7F3DCCB8"
+#define TOKEN "9640121413"
+//-------- Customise the above values --------
+
+char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
+char topic[] = "iot-2/evt/status/fmt/json";
+char authMethod[] = "use-token-auth";
+char token[] = TOKEN;
+char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
+
+WiFiClient wifiClient;
+PubSubClient client(server, 1883, NULL, wifiClient);
+
+void setup() {
+ Serial.begin(115200);
+ Serial.println();
+
+ Serial.print("Connecting to "); Serial.print(ssid);
+ WiFi.begin(ssid, password);
+ while (WiFi.status() != WL_CONNECTED) {
+ delay(500);
+ Serial.print(".");
+ } 
+ Serial.println("");
+
+ Serial.print("WiFi connected, IP address: "); Serial.println(WiFi.localIP());
+}
+
+int counter = 0;
+
+void loop() {
+
+ if (!client.connected()) {
+ Serial.print("Reconnecting client to ");
+ Serial.println(server);
+ while (!client.connect(clientId, authMethod, token)) {
+ Serial.print(".");
+ delay(500);
+ }
+ Serial.println();
+ }
+
+ String payload = "{\"d\":{\"Name\":\"18FE34D81E46\"";
+ payload += ",\"counter\":";
+ payload += counter++;
+ payload += "}}";
+ 
+ Serial.print("Sending payload: ");
+ Serial.println(payload);
+ 
+ if (client.publish(topic, (char*) payload.c_str())) {
+ Serial.println("Publish ok");
+ } else {
+ Serial.println("Publish failed");
+ }
+
+ delay(5000);
+}
+
